47b291d8f3848122c285ecbf3aaaa9f332d3d537
Diego Carreon <153489650+dg24crn@users.noreply.github.com>
2024-11-25 05:56:04 -0600

    Update Navbar.tsx

--- front/src/components/navbar/Navbar.tsx
+++ front/src/components/navbar/Navbar.tsx
@@ -1,15 +1,11 @@
-// src/components/Navbar.tsx
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#E5E7EB] shadow-md py-4">
+    <nav className="bg-[#E5E7EB] shadow-md py-4 border border-black">
       <div className="container mx-auto px-4 flex items-center justify-between">
         {/* Add Logo Later */}
         <div className="flex items-center space-x-2">
-          <div className="w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center text-white font-bold text-xl">
-            M
-          </div>
           <span className="text-gray-900 text-lg font-bold hidden sm:block">
             Managé
           </span>
